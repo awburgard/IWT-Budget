d82e81f6180b5dff5dd8062a3ff0aa9e564686e1
awburgard <awburgard@gmail.com>
2019-11-23 09:09:44 -0600

    Updates scripts to run server and client

---
+++ src/components/App/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Calc from '../Calc/Calc';
+
+function App() {
+  return (
+   <div>
+     <Calc />
+   </div>
+  );
+}
+
+export default App;
---
+++ src/components/Calc/Calc.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Calc extends Component {
+    render(){
+        return(
+            <div>
+                <h1>Percentage Breakdown</h1>
+            </div>
+        )
+    }
+}
+
+export default Calc
